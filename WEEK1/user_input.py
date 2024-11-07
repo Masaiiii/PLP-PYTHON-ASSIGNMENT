e4742e18d642e28d6fda80d809dd360d235cd3e1
masaiiii <zakanjamaasai@gmail.com>
2024-11-07 12:29:44 +0300

    commit

--- WEEK1/user_input.py
+++ WEEK1/user_input.py
@@ -2,4 +2,4 @@ name = input("What is your full name? ")
 age = input("How old are you? ")
 location = input("Where do you live? ")
 
-print(f"Your name is {name} in , you are {age} years old and you live in {location}")
+print(f"Your name is {name}  , you are {age} years old and you live in {location}")
